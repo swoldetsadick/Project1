430930e18a7ee434e1c12a2b3e54f7cc62910b47
swoldetsadick <swoldetsadick@ineteconomics.org>
2014-09-25 22:21:39 +0200

    message

--- project1.R
+++ project1.R
@@ -2,7 +2,7 @@
 setwd("~/Downloads/Desktop/Projects/Project1")
 french <- read.csv("~/Downloads/Desktop/Projects/Project1/french.csv", header = TRUE, sep = ",", fileEncoding="latin1")
 
-#I???m going to make a completely arbitrary decision here and choose to categorize 
+#I am going to make a completely arbitrary decision here and choose to categorize 
 #each adjective as positive or negative based on their weight/score.
 french$pos <- french$POS.FREQ >= .6
 
@@ -18,3 +18,4 @@ neg.words <- neg.words[,1]
 
 write.table(pos.words, file="./poswords.txt", row.names = FALSE, col.names = TRUE, quote = FALSE, fileEncoding="latin1")
 write.table(neg.words, file="./negwords.txt", row.names = FALSE, col.names = TRUE, quote = FALSE, fileEncoding="latin1")
+geocode(47, 2, 293km)
